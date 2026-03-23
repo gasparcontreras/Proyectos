d94e6312d282992f1e46beace75fdb6cf567abf6
gasparcontreras <73745033+gasparcontreras@users.noreply.github.com>
2026-03-22 20:43:06 -0500

    3

---
+++ EJEMPLO/EJEMPLO2/EJEMPLO 3/05Cadenas.py
@@ -0,0 +1,8 @@
+#Extraer texto en base a una posición indicada
+mensaje = 'Bienvenidos alumnos del curso de Python'
+subcadena = mensaje[23:32]
+print('La subcadena extraída es',subcadena)
+
+'''
+Rebanado o "slicing"
+'''
