03923bfd1d10ff3781d20d8736a6def30919b5b8
gasparcontreras <73745033+gasparcontreras@users.noreply.github.com>
2026-03-22 20:41:00 -0500

    PRIMERA VEZ

---
+++ EJEMPLO/01HolaMundo.py
@@ -0,0 +1,10 @@
+#Imprimiento valores
+print('Hola Mundo')
+print(1+1)
+# control + enter - > ejecutar celda actual
+# shift + enter -> ejecutar celda actual y posicionarse en la siguiente celda
+1+1
+
+# Ingreso de valores
+valor=input("Ingrese el dato: ")
+valor
